efc4215fcda97c7e798d64928967b05e152b9c98
Krista <krista.abraham@gmail.com>
2018-04-28 21:28:36 -0400

    Remove logs

--- client/src/alert.jsx
+++ client/src/alert.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 export const Alert = (props) => {
-  console.log('Alert props', props) // num tasks, today/tomorrow
-
   return (
     <Card
-      //href=''
       color="yellow"
       header={props.num}
       description={`tasks due ${props.day}`}
